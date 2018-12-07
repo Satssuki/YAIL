f7341e0fd2deede09a0418f5d897f00ba01bc82b
William Garneau <willi_garn@hotmail.com>
2018-12-07 16:54:08 -0500

    Fixed border when adding paddin

--- Vision/DataExtractor.cpp
+++ Vision/DataExtractor.cpp
@@ -33,6 +33,7 @@ std::vector<cv::Mat> DataExtractor::StepCharacter(cv::Mat & input) {
 	std::vector<cv::Mat> characArray;
 	cvtColor(input, gray, CV_BGR2GRAY);
 	threshold(gray, gray, 200, 255, cv::THRESH_BINARY_INV);
+	RNG rng(12345);
 
 	std::vector<std::vector<cv::Point>> contours;
 	std::vector<cv::Vec4i> hierarchy;
@@ -44,9 +45,11 @@ std::vector<cv::Mat> DataExtractor::StepCharacter(cv::Mat & input) {
 	{
 		cv::Mat charac;
 		cv::Rect r = boundingRect(contours[i]);
-		rectangle(input, cv::Point(r.x - padding, r.y - padding), cv::Point(r.x + r.width + padding, r.y + r.height + padding), Scalar(0, 0, 255), 1, 8, 0);
-		gray(cv::Rect(cv::Point(r.x - padding, r.y - padding), cv::Point(r.x + r.width + padding, r.y + r.height + padding))).copyTo(charac);
+		rectangle(input, cv::Point(r.x - padding, r.y), cv::Point(r.x + r.width, r.y + r.height), Scalar(0, 0, 255), 1, 8, 0);
+		gray(cv::Rect(cv::Point(r.x - padding, r.y), cv::Point(r.x + r.width, r.y + r.height))).copyTo(charac);
+		cv::copyMakeBorder(charac, charac, 20, 20, 20, 20, BORDER_CONSTANT, cv::Scalar(0,0,0));
 		cv::resize(charac, charac, cv::Size(28, 28));
+		cv::waitKey(0);
 		characArray.push_back(charac);
 	}
 	return characArray;
