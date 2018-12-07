1b83568c3741d28a88fe3f77958e70b9a7915ec9
William Garneau <willi_garn@hotmail.com>
2018-12-07 11:17:24 -0500

    Extracting data

--- Vision/DataExtractor.cpp
+++ Vision/DataExtractor.cpp
@@ -9,6 +9,7 @@ std::vector<cv::Mat> DataExtractor::ExtractCharacters(cv::Mat & input) {
 	std::vector<cv::Vec4i> hierarchy;
 
 	cv::findContours(gray, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
+	std::sort(contours.begin(), contours.end(), contour_sorter());
 	cv::Rect minRect;
 
 	if (contours.size() > 0) {
@@ -19,7 +20,6 @@ std::vector<cv::Mat> DataExtractor::ExtractCharacters(cv::Mat & input) {
 				minRect.y = 0;
 				temp = input(minRect);
 				temp = cv::Scalar(255, 255, 255);
-				cv::imshow("test", input);
 				StepCharacter(input);
 				cv::waitKey(0);
 			}
@@ -29,7 +29,7 @@ std::vector<cv::Mat> DataExtractor::ExtractCharacters(cv::Mat & input) {
 }
 
 void DataExtractor::StepCharacter(cv::Mat & input) {
-	cv::Mat gray;
+	cv::Mat gray, test;
 	cvtColor(input, gray, CV_BGR2GRAY);
 	threshold(gray, gray, 200, 255, cv::THRESH_BINARY_INV);
 
@@ -37,13 +37,18 @@ void DataExtractor::StepCharacter(cv::Mat & input) {
 	std::vector<cv::Vec4i> hierarchy;
 
 	findContours(gray, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
-	int padding = 5;
+	std::sort(contours.begin(), contours.end(), contour_sorter());
+	int padding = 3;
 	for (int i = 0; i < contours.size(); i = hierarchy[i][0])
 	{
+		cv::Mat charac;
 		cv::Rect r = boundingRect(contours[i]);
-		cv::rectangle(input, cv::Point(r.x - padding, r.y - padding), cv::Point(r.x + r.width + padding, r.y + r.height + padding), cv::Scalar(0, 0, 255), 1, 8, 0);
+		rectangle(input, cv::Point(r.x - padding, r.y - padding), cv::Point(r.x + r.width + padding, r.y + r.height + padding), Scalar(0, 0, 255), 1, 8, 0);
+		input(cv::Rect(cv::Point(r.x - padding, r.y - padding), cv::Point(r.x + r.width + padding, r.y + r.height + padding))).copyTo(charac);
+		cv::resize(charac, charac, cv::Size(28, 28));
+		cv::imshow("Test", charac);
+		cv::waitKey(0);
 	}
-
-	cv::imshow("Test", input);
+	imshow("result", input);
 	cv::waitKey(0);
 }
--- Vision/DataExtractor.h
+++ Vision/DataExtractor.h
@@ -1,11 +1,26 @@
 #pragma once
 #include "AllIncludes.h"
+
+using namespace cv;
+using namespace std;
+
 class DataExtractor
 {
 public:
 	static std::vector<cv::Mat> ExtractCharacters(cv::Mat &input);
 	static void StepCharacter(cv::Mat &input);
 private:
-	static std::vector<cv::Mat> characters;
+
+
+	struct contour_sorter // 'less' for contours
+	{
+		bool operator ()(const vector<Point>& a, const vector<Point> & b)
+		{
+			Rect ra(boundingRect(a));
+			Rect rb(boundingRect(b));
+			// scale factor for y should be larger than img.width
+			return ((ra.x + 1000) < (rb.x + 1000));
+		}
+	};
 };
 
